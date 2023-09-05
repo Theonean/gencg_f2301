c0f2f78541fbaebfdda6fd914af06295edd35992
NailKnight <robinwelmers@protonmail.com>
2023-09-05 13:29:17 +0200

    trying around with numbers, partyversion

--- content/day02/rotatingSquares/sketch.js
+++ content/day02/rotatingSquares/sketch.js
@@ -3,19 +3,38 @@ let canvas;
 // Default P5 setup function
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
+
+}
+
+let movingX = 0;
+let movingY = 0;
+let squareSide = 100;
+let offsetX = -squareSide * 4;
+let offsetY = -squareSide * 3;
+let squareInbetweenDistance = squareSide * 1.1;
+let innerRotationMultiplier = 2;
+let columns = 20;
+let rows = 12;
+let timeSpeed = 0.01;
+
+// Time variable
+let t = 0;
+
+function draw() {
+    clear();
     background(color(0));
 
-    let offsetX = -200;
-    let offsetY = 0;
+    // Change innerRotationMultiplier over time
+    innerRotationMultiplier = sin(t) * 3.0;
+
     //r = row
-    for (let r = 0; r < 12; r++) {
+    for (let r = 0; r < rows; r++) {
         //c = column, starts with 2 so everything has atleast 1 inner square
-        for (let c = 2; c < 18; c++) {
+        for (let c = 2; c < columns; c++) {
 
             //Offset moves the grid around
-            let squareX = c * 100 + offsetX;
-            let squareY = r * 100 + offsetY;
-            let squareSide = 70;
+            let squareX = c * squareInbetweenDistance + offsetX + movingX;
+            let squareY = r * squareInbetweenDistance + offsetY + movingY;
 
             let innerSquareSide = squareSide;
 
@@ -23,13 +42,16 @@ function setup() {
             let innerOffsetY = 0;
 
             //Draw inner squares
-            for (let cRec = 1; cRec < c; cRec++) {
+            for (let cRec = 1; cRec < Math.random() * 5; cRec++) {
                 //recursive inner squares become smaller
                 innerSquareSide *= 0.8;  // adjust this factor as you like
                 innerOffsetX = (squareSide - innerSquareSide) / 2;
                 innerOffsetY = (squareSide - innerSquareSide) / 2;
 
-                let squareTopLeftX = squareX + innerOffsetX;
+                //Change these variables over time
+
+
+                let squareTopLeftX = squareX + innerOffsetX; //moves line over time
                 let squareTopLeftY = squareY + innerOffsetY;
                 let squareTopRightX = squareTopLeftX + innerSquareSide;
                 let squareTopRightY = squareTopLeftY;
@@ -37,25 +59,55 @@ function setup() {
                 let squareBottomLeftY = squareTopLeftY + innerSquareSide;
                 let squareBottomRightX = squareTopRightX;
                 let squareBottomRightY = squareBottomLeftY;
-                let rotation = cRec * 1.3;
-        
+                let rotation = cRec * innerRotationMultiplier;
+
                 strokeWeight(2);
                 stroke(255);
-        
+
                 push();
                 translate(squareX + innerOffsetX + innerSquareSide / 2, squareY + innerOffsetY + innerSquareSide / 2);
-                rotate(rotation);
+                if (c % 2 == r % 2) {
+                    rotate(rotation);
+                }
+                else {
+                    rotate(-rotation)
+                }
                 translate(-squareX - innerOffsetX - innerSquareSide / 2, -squareY - innerOffsetY - innerSquareSide / 2);
-                line(squareTopLeftX, squareTopLeftY, squareTopRightX, squareTopRightY);
-                line(squareTopLeftX, squareTopLeftY, squareBottomLeftX, squareBottomLeftY);
-                line(squareBottomRightX, squareBottomRightY, squareBottomLeftX, squareBottomLeftY);
-                line(squareBottomRightX, squareBottomRightY, squareTopRightX, squareTopRightY);
+
+                fill(cRec * 10, cRec * 3, cRec * 50);
+
+                // Draw a filled square
+                beginShape();
+                vertex(squareTopLeftX, squareTopLeftY);
+                vertex(squareTopRightX, squareTopRightY);
+                vertex(squareBottomRightX, squareBottomRightY);
+                vertex(squareBottomLeftX, squareBottomLeftY);
+                endShape(CLOSE);
                 pop();
                 //square(squareX + innerOffsetX, squareY + innerOffsetY, innerSquareSide);
             }
 
         }
     }
+
+    //Change variables over time
+    //Rotate over time
+    //innerRotationMultiplier = innerRotationMultiplier < PI ? innerRotationMultiplier + 0.01 : 0;
+
+    //opens up square
+
+    // Change movingX and movingY over time (or however you'd like to update them)
+    movingX += cos(t) * 0.5;
+    movingY += sin(t) * 0.5;
+
+    // Increment time variable
+    t += timeSpeed;
+
+    let nextX = movingX;
+    let nextY = movingY;
+    nextX = constrain(nextX, 0, windowWidth);
+    nextY = constrain(nextY, 0, windowHeight);
+
+    movingX = nextX;
+    movingY = nextY;
 }
-
-
